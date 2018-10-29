ef010d473b010bd4f549ec7c2b38703faf0d95ee
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2018-10-29 22:32:05 +0100

    Expanded publisher.py to publish stdin to a ROS topic.

--- src/publisher/publisher.py
+++ src/publisher/publisher.py
@@ -1,6 +1,26 @@
-# TODO: Implement ROS-node to publish words on a topic.
+# TODO: Do something more than just echo stdin to the ROS topic.
 
 import sys
+import argparse
+import rospy
+import std_msgs
 
-for line in sys.stdin:
-    print(line)
+
+def main(FLAGS, args):
+    rospy.init_node('voice_recognition')
+    pub = rospy.Publisher('/ai/voice/command',
+                          std_msgs.msg.String, queue_size=1)
+
+    line = ''
+    while not rospy.is_shutdown():
+        try:
+            line = input()
+        except EOFError:
+            break
+        pub.publish(line)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    FLAGS, unknown = parser.parse_known_args()
+    main(FLAGS, unknown)
