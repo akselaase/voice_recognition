cf64bf5bd0f7d11612913b6198fd50872635f1cb
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2019-01-13 13:38:06 +0100

    Pointed training script to ascend dataset

--- src/labeler/train.sh
+++ src/labeler/train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo Reading wanted words from ./wanted_words.txt
+echo Training model to recognize "$(cat wanted_words.txt)"
 python3.6 ./tf/train.py \
     --wanted_words="$(cat ./wanted_words.txt)" \
-    --max_samples_per_word=500 \
+    --max_samples_per_word=5000 \
     --testing_percentage=10 \
     --validation_percentage=10 \
-    --data_dir=../data/aiy_speech_dataset \
+    --data_dir=../../data/samples/ascend \
     --summaries_dir=/tmp/retrain_logs \
     --train_dir=/tmp/speech_commands_train \
     --start_checkpoint=$1
