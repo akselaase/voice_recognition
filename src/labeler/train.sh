8ad0c965f6421d045cdc6439a58979fbaa3a4f0f
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2019-02-05 21:07:01 +0100

    Updated train.sh script to point to use ARSE samples in training

--- src/labeler/train.sh
+++ src/labeler/train.sh
@@ -2,10 +2,12 @@
 echo Training model to recognize "$(cat wanted_words.txt)"
 python ./tf/train.py \
     --wanted_words="$(cat ./wanted_words.txt)" \
-    --max_samples_per_word=5000 \
+    --max_samples_per_word=-1 \
     --testing_percentage=10 \
     --validation_percentage=10 \
-    --data_dir=../../data/samples/ascend \
-    --summaries_dir=/tmp/retrain_logs \
-    --train_dir=/tmp/speech_commands_train \
+    --unknown_percentage=25 \
+    --data_url= \
+    --data_dir=../../data/samples/arse \
+    --summaries_dir=training/retrain_logs \
+    --train_dir=training/speech_commands_train \
     --start_checkpoint=$1
