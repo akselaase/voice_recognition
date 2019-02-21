4750c1c5412e956670991fb024f24e41f7e074ed
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2019-02-21 08:50:34 +0100

    Modified run-script to use sox record instead of arecord, and added variables for ROS master URI, python binary and audio input device

--- bin/run.sh
+++ bin/run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 export TF_CPP_MIN_LOG_LEVEL=3
+export ROS_MASTER_URI=http://localhost:11311/
+export AUDIODEVICE=plughw:CARD=UR22,DEV=0
 MODEL=arse
+#RECORDER="arecord -t wav -r 16000 -c 1 -f S16_LE -d 0 -N -- -"
+RECORDER="rec -q -r 16000 -b 16 -c 1 -e signed-integer --endian little -t wav -"
+PYTHON=python3.5
 
 cat ../data/models/$MODEL/desc.txt
-arecord -t wav -r 16000 -c 1 -f S16_LE -d 0 -N -- - | \
-    python -u ../src/splitter/splitter.py -t 0.5,0.05 | \
-    python -u ../src/labeler/label_wav.py \
+
+$RECORDER | \
+    $PYTHON -u ../src/splitter/splitter.py -t 5.0,1.0 | \
+    $PYTHON -u ../src/labeler/label_wav.py \
         --graph=../data/models/$MODEL/graph.pb \
         --labels=../data/models/$MODEL/conv_labels.txt \
         --num_outputs=2 | \
-    python -u ../src/filter/filter.py # | \
-    #python -u ../src/publisher/publisher.py
+    $PYTHON -u ../src/filter/filter.py  | \
+    $PYTHON -u ../src/publisher/publisher.py
