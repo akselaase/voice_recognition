9bc7dd4bc5a620baa1c3b5c624bcc18a7a1ed904
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2019-02-25 21:01:05 +0100

    Adjust training parameters

--- src/labeler/train.sh
+++ src/labeler/train.sh
@@ -6,13 +6,15 @@ python ./tf/train.py \
     --testing_percentage=10 \
     --validation_percentage=10 \
     --unknown_percentage=15 \
-    --silence_percentage=15 \
+    --silence_percentage=10 \
     --batch_size=16 \
+    --background_volume=0.3 \
     --data_url= \
     --data_dir=../../data/samples/arse_augmented \
     --summaries_dir=training/retrain_logs \
     --train_dir=training/speech_commands_train \
     --start_checkpoint=$1 \
     --how_many_training_steps=200000,20000 \
+    --learning_rate=0.001,0.0001 \
     --save_step_interval=4000 \
     --eval_step_interval=4000
