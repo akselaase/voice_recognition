c609e2f067cdd09ff96ba03a767d7f4954fa8c06
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2019-05-01 20:53:38 +0200

    Added check for python packages pyyaml and rospkg, and changed exit codes

--- bin/check_deps.sh
+++ bin/check_deps.sh
@@ -18,19 +18,21 @@ fi
 
 # Test python modules
 
+$PYTHON -c "import yaml" >/dev/null 2>&1 && echo "Found yaml" || error 0 "Missing package pyyaml"
+$PYTHON -c "import rospkg" >/dev/null 2>&1 && echo "Found rospkg" || error 0 "Missing package rospkg"
 $PYTHON -c "import rospy" >/dev/null 2>&1 && echo "Found rospy" || error 0 "Missing package rospy"
-$PYTHON -c "import std_msgs" >/dev/null 2>&1 && echo "Found std_msgs" || error 1 "Missing package std_msgs"
-$PYTHON -c "import tensorflow" >/dev/null 2>&1 && echo "Found tensorflow" || error 2 "Missing package tensorflow(-gpu)"
+$PYTHON -c "import std_msgs" >/dev/null 2>&1 && echo "Found std_msgs" || error 0 "Missing package std_msgs"
+$PYTHON -c "import tensorflow" >/dev/null 2>&1 && echo "Found tensorflow" || error 0 "Missing package tensorflow(-gpu)"
 if [ $err -ne 0 ]; then
 	echo "Make sure to activate a virtualenv with tensorflow-gpu, rospy, rospkg, and pyyaml installed."
 fi
 # Test binaries
 
-which sox >/dev/null 2>&1 && echo "Found sox" || error 3 "Missing sox"
+which sox >/dev/null 2>&1 && echo "Found sox" || error 1 "Missing sox"
 
 # Test local IP address retrieval
 IP="$(ip route get 1 | awk '{print $7;exit}')"
-[[ $IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] && echo "Successfully parsed local IP" || error 4 "Failed to retrieve local IP,
+[[ $IP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] && echo "Successfully parsed local IP" || error 2 "Failed to retrieve local IP,
     you may need to manually edit ROS_IP in bin/run.sh"
 
 if [ "$err" -eq "0" ]; then
