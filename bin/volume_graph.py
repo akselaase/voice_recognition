0559bccaf18652c332198fb6ed34fbeda4860835
Aksel Lunde Aase <aksel.lunde.aase@gmail.com>
2019-05-02 08:29:53 +0200

    Changed y-axis scale in volume visualizer to match thresholds.txt scale

--- bin/volume_graph.py
+++ bin/volume_graph.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot(h, l, width=1600):
-    plt.plot(np.array(pickle.load(open('sums.bin', 'rb')))/32768**2/width)
-    plt.axhline(l/100)
-    plt.axhline(h/100)
+    plt.plot(100 * np.array(pickle.load(open('sums.bin', 'rb')))/32768**2/width)
+    plt.axhline(l)
+    plt.axhline(h)
     plt.show()
 
 hi, lo = map(float, open('thresholds.txt').read().split(','))
